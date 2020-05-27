db811956f951a784713a5ded8cb65f14f34e17fe
Nennadd <nenadj372@gmail.com>
2020-05-27 17:54:46 +0200

    Commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+const slides = document.querySelector(".slider").children;
+console.log(slides);
