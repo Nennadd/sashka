ee27db713fc04abf99f06e7947cf6e8b4530b1f9
Nennadd <nenadj372@gmail.com>
2020-05-27 19:15:45 +0200

    Commit

--- js/app.js
+++ js/app.js
@@ -7,31 +7,39 @@ const slides = document.querySelector(".slider").children;
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 let index = 0;
-
+let intervalID;
 prev.addEventListener("click", () => {
-  console.log(prevSlide());
+  prevSlide();
 });
 next.addEventListener("click", () => {
   nextSlide();
-  changeSlide();
 });
 
 function prevSlide() {
-  if (index === 0) return (index = 2);
-  return --index;
+  if (index === 0) {
+    index = 2;
+  } else {
+    index--;
+  }
+  changeSlide();
 }
 
 function nextSlide() {
-  if (index === slides.length - 1) return (index = 0);
-  return ++index;
+  if (index === slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeSlide();
 }
 
 function changeSlide() {
   for (let i = 0; i < slides.length; i++) {
-    if (i === index) {
-      slides[i].classList.add("active");
-    } else {
-      slides[i].classList.remove("active");
-    }
+    slides[i].classList.remove("active");
   }
+  slides[index].classList.add("active");
 }
+
+// intervalID = setInterval(() => {
+//   nextSlide();
+// }, 4000);
