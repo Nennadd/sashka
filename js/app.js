82e0af6f0ea4c0335bca7a5d359054e2073b93d8
Nennadd <nenadj372@gmail.com>
2020-05-27 18:56:50 +0200

    Commit

--- js/app.js
+++ js/app.js
@@ -4,4 +4,34 @@ button.addEventListener("click", (e) => {
 });
 
 const slides = document.querySelector(".slider").children;
-console.log(slides);
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+let index = 0;
+
+prev.addEventListener("click", () => {
+  console.log(prevSlide());
+});
+next.addEventListener("click", () => {
+  nextSlide();
+  changeSlide();
+});
+
+function prevSlide() {
+  if (index === 0) return (index = 2);
+  return --index;
+}
+
+function nextSlide() {
+  if (index === slides.length - 1) return (index = 0);
+  return ++index;
+}
+
+function changeSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    if (i === index) {
+      slides[i].classList.add("active");
+    } else {
+      slides[i].classList.remove("active");
+    }
+  }
+}
